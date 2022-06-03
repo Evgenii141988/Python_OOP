376ce83be738b7afd7d1d43c82ad87f36d473666
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-03 15:19:57 +0300

    Create new task1_2_5.py

---
+++ Python_OOP_training_course_Sergey_Balakirev/task1_2_5.py
@@ -0,0 +1,15 @@
+# Подвиг 5. Объявите пустой класс с именем Car. С помощью функции setattr() добавьте в этот класс атрибуты:
+# model: "Тойота"
+# color: "Розовый"
+# number: "О111АА77"
+# Выведите на экран атрибут color, используя словарь __dict__ класса Car.
+
+class Car:
+    pass
+
+
+if __name__ == '__main__':
+    setattr(Car, 'model', "Тойота")
+    setattr(Car, 'color', "Розовый")
+    setattr(Car, 'number', "О111АА77")
+    print(Car.__dict__['color'])
