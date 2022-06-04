9553f0978bb8334db342ea6af157b57b354856fa
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-04 13:40:10 +0300

    1.3 Методы классов. Параметр self

--- Python_OOP_training_course_Sergey_Balakirev/1_2classes_objects_attributes.py
+++ Python_OOP_training_course_Sergey_Balakirev/1_2classes_objects_attributes.py
@@ -3,6 +3,13 @@ class Point:
     color = 'red'
     circle = 2
 
+    def set_coords(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_coords(self):
+        return self.x, self.y
+
 
 if __name__ == '__main__':
     Point.color = 'black'
@@ -12,3 +19,7 @@ if __name__ == '__main__':
     a.y = 2
     b.x = 10
     b.y = 20
+    pt = Point()
+    pt.set_coords(1, 3)
+    print(pt.__dict__)
+    print(pt.get_coords())
