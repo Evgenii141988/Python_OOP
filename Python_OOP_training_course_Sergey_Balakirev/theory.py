513b3434fa1e2a9e119cc1fce8cf9d545c628dbe
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-06 15:18:41 +0300

    1.4 Инициализатор __init__ и финализатор __del__

--- Python_OOP_training_course_Sergey_Balakirev/theory.py
+++ Python_OOP_training_course_Sergey_Balakirev/theory.py
@@ -3,6 +3,10 @@ class Point:
     color = 'red'
     circle = 2
 
+    def __init__(self, a, b):
+        self.x = a
+        self.y = b
+
     def set_coords(self, x, y):
         self.x = x
         self.y = y
@@ -10,16 +14,19 @@ class Point:
     def get_coords(self):
         return self.x, self.y
 
+    def __del__(self):
+        print(f'Удаление экземпляра класса{self}')
+
 
 if __name__ == '__main__':
     Point.color = 'black'
-    a = Point()
-    b = Point()
+    a = Point(1, 2)
+    b = Point(3, 4)
     a.x = 1
     a.y = 2
     b.x = 10
     b.y = 20
-    pt = Point()
+    pt = Point(5, 6)
     pt.set_coords(1, 3)
     print(pt.__dict__)
     print(pt.get_coords())
