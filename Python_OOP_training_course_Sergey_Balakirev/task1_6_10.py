7ba86b7d212ab8cdf8304ae44bb9a422b81cad17
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-11 14:22:38 +0300

    1.6 new task 10

---
+++ Python_OOP_training_course_Sergey_Balakirev/task1_6_10.py
@@ -0,0 +1,27 @@
+class AppStore:
+    APPS = []
+
+    def add_application(self, app):
+        self.APPS.append(app)
+
+    def remove_application(self, app):
+        self.APPS.remove(app)
+
+    def block_application(self, app):
+        app.blocked = True
+
+    def total_apps(self):
+        return len(self.APPS)
+
+
+class Application:
+    def __init__(self, name, blocked=False):
+        self.name = name
+        self.blocked = blocked
+
+
+if __name__ == '__main__':
+    store = AppStore()
+    app_youtube = Application("Youtube")
+    store.add_application(app_youtube)
+    store.remove_application(app_youtube)
