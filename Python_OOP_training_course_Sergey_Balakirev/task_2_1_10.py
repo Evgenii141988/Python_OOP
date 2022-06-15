b9f4fbf714aeeb9fbbf0d48d4efe2a9613a58e29
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-15 20:15:50 +0300

    2.1 new task 9 Большой подвиг 9. Необходимо реализовать связный список, когда объекты класса ObjList связаны с соседними через приватные свойства __next и __prev:Для этого объявите класс LinkedList, который будет представлять связный список в целом и иметь набор следующих методов:
    add_obj(self, obj) - добавление нового объекта obj класса ObjList в конец связного списка;
    remove_obj(self) - удаление последнего объекта из связного списка;
    get_data(self) - получение списка из строк локального свойства __data всех объектов связного списка.
    
    И локальные публичные атрибуты:
    
    head - ссылка на первый объект связного списка (если список пустой, то head = None);
    tail - ссылка на последний объект связного списка (если список пустой, то tail = None).
    
    Объекты класса ObjList должны иметь следующий набор приватных локальных свойств:
    
    __next - ссылка на следующий объект связного списка (если следующего объекта нет, то __next = None);
    __prev - ссылка на предыдущий объект связного списка (если предыдущего объекта нет, то __prev = None);
    __data - строка с данными.
    
    Также в классе ObjList должны быть реализованы следующие сеттеры и геттеры:
    
    set_next(self, obj) - изменение приватного свойства __next на значение obj;
    set_prev(self, obj) - изменение приватного свойства __prev на значение obj;
    get_next(self) - получение значения приватного свойства __next;
    get_prev(self) - получение значения приватного свойства __prev;
    set_data(self, data) - изменение приватного свойства __data на значение data;
    get_data(self) - получение значения приватного свойства __data.

---
+++ Python_OOP_training_course_Sergey_Balakirev/task_2_1_10.py
@@ -0,0 +1,38 @@
+import re
+from random import choice, randint
+from string import ascii_letters, digits
+
+
+class EmailValidator:
+    def __new__(cls, *args, **kwargs):
+        return None
+
+    @classmethod
+    def check_email(cls, email):
+        reg = r'^[a-zA-Z_\d.]{0,100}@[a-zA-Z_\d\.]{1,50}\.[a-zA-Z_\d\.]{1,50}'
+        if cls.__is_email_str(email):
+            if re.fullmatch(reg, email) and '..' not in email and len(email) <= 151:
+                return True
+        return False
+
+    @classmethod
+    def get_random_email(cls):
+        random_number = randint(1, 100)
+        email = ''
+        while len(email) <= random_number:
+            letter = choice(ascii_letters + digits + '_.')
+            email += letter
+        return f'{email}@gmail.com'
+
+    @staticmethod
+    def __is_email_str(email):
+        return isinstance(email, str)
+
+
+if __name__ == '__main__':
+    em = EmailValidator()
+    res = EmailValidator.check_email("sc_lib@list.ru")  # True
+    res1 = EmailValidator.check_email("sc_lib@list_ru")  # False
+    print(res)
+    print(res1)
+    print(EmailValidator.get_random_email())
