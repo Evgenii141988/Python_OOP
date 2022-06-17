2b01c8905a12c5b2dbe37256828192a2d454124c
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-17 13:57:47 +0300

    change task 2.2.6

--- Python_OOP_training_course_Sergey_Balakirev/task_2_2_6.py
+++ Python_OOP_training_course_Sergey_Balakirev/task_2_2_6.py
@@ -17,17 +17,57 @@ class StackObj:
 
     @next.setter
     def next(self, obj: object):
-        if isinstance(obj, (StackObj, None)):
+        if isinstance(obj, StackObj) or obj is None:
             self.__next = obj
 
 
 class Stack:
-    def __init__(self, top=None):
-        self.top = top
+    def __init__(self):
+        self.top = None
+        self.__end = None
 
     def push(self, obj: object):
         if self.top is None:
             self.top = obj
+            self.__end = obj
         else:
-            self.top.next =
+            self.__end.next = obj
+            self.__end = obj
 
+    def pop(self):
+        objs = []
+        obj = self.top
+        while obj:
+            objs.append(obj)
+            obj = obj.next
+        try:
+            obj = objs.pop()
+            self.__end = objs[-1]
+            self.__end.next = None
+        except IndexError:
+            self.top = None
+            self.__end = None
+        return obj
+
+    def get_data(self):
+        datas = []
+        obj = self.top
+        while obj:
+            datas.append(obj.data)
+            obj = obj.next
+        return datas
+
+
+if __name__ == '__main__':
+    st = Stack()
+    st.push(StackObj("obj1"))
+    st.push(StackObj("obj2"))
+    st.push(StackObj("obj3"))
+    print(st.get_data())
+    print(st.pop().data)
+    print(st.pop().data)
+    print(st.get_data())
+    print(st._Stack__end.data)
+    print(st.pop().data)
+    print(st.top)
+    print(st._Stack__end)
