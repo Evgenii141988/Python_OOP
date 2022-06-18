773b5bdbd228225d284851012c773c20c1c352d2
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-18 13:53:06 +0300

    2.3 Дескрипторы (data descriptor и non-data descriptor)

--- Python_OOP_training_course_Sergey_Balakirev/theory_data_and_non_data_descriptor.py
+++ Python_OOP_training_course_Sergey_Balakirev/theory_data_and_non_data_descriptor.py
@@ -1,19 +1,12 @@
 class Integer:
     def __set_name__(self, owner, name):
-        print(owner)
-        print(name)
         self.name = f'_{name}'
-        print(self.name)
 
     def __set__(self, instance, value):
-        print(instance)
-        print(value)
         print(f'__set__: {self.name} = {value}')
         setattr(instance, self.name, value)
 
     def __get__(self, instance, owner):
-        print(instance)
-        print(owner)
         print(f'__get__: {self.name}')
         return getattr(instance, self.name)
 
