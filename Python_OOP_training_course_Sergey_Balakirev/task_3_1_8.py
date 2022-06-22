a56a559e04eba647bcf6cec79c194df898c719c8
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-22 11:47:24 +0300

    task 3.1.8 верное решение Подвиг 8. Объявите класс Circle (окружность), объекты которого должны создаваться командой:
    circle = Circle(x, y, radius)   # x, y - координаты центра окружности; radius - радиус окружности
    В каждом объекте класса Circle должны формироваться локальные приватные атрибуты:
    
    __x, __y - координаты центра окружности (вещественные или целые числа);
    __radius - радиус окружности (вещественное или целое положительное число).
    
    Для доступа к этим приватным атрибутам в классе Circle следует объявить объекты-свойства (property):
    
    x, y - для изменения и доступа к значениям __x, __y, соответственно;
    radius - для изменения и доступа к значению __radius.
    
    При изменении значений приватных атрибутов через объекты-свойства нужно дополнительно проверять, что присваиваемые значения - числа (целые или вещественные). Дополнительно у радиуса проверять, что число должно быть положительным (строго больше нуля). Сделать это нужно через магические методы. При некорректных переданных значениях, прежние значения меняться не должны.
    
    Если присваиваемое значение не числовое, то генерировать исключение командой:
    
    raise TypeError("Неверный тип присваиваемых данных.")
    Эту же проверку через объекты-свойства x, y, radius делать в инициализаторе. Если проверка не проходит, то генерируется указанное исключение TypeError.
    
    При обращении к несуществующему атрибуту объектов класса Circle выдавать булево значение False.

--- Python_OOP_training_course_Sergey_Balakirev/task_3_1_8.py
+++ Python_OOP_training_course_Sergey_Balakirev/task_3_1_8.py
@@ -26,16 +26,16 @@ class Circle:
 
     @radius.setter
     def radius(self, value):
-        print('setter radius')
         self.__radius = value
 
     def __setattr__(self, key, value):
-        print(f'__setter__: {key}')
-        if not ((key in ('_Circle__x', '_Circle__y', 'x', 'y') and isinstance(value, (int, float))) or (
-                key in ('radius') and isinstance(value, (int, float)) and value > 0)):
+        if not isinstance(value, (int, float)):
             raise TypeError("Неверный тип присваиваемых данных.")
-        if not (key in ('_Circle__radius', 'radius') and value <= 0):
-            super().__setattr__(key, value)
+        if key == 'radius' and value <= 0:
+            return
+        if key == '_Circle__radius' and value <= 0:
+            raise TypeError("Неверный тип присваиваемых данных.")
+        super().__setattr__(key, value)
 
     def __getattr__(self, item):
         return False
@@ -43,10 +43,6 @@ class Circle:
 
 if __name__ == '__main__':
     circle = Circle(10.5, 7, 22)
-    print(circle.x)
-    circle.x = 6
     circle.radius = -10  # прежнее значение не должно меняться, т.к. отрицательный радиус недопустим
     x, y = circle.x, circle.y
-    # circle.radius = 10
-    print(circle.radius)
     res = circle.name  # False, т.к. атрибут name не существует
