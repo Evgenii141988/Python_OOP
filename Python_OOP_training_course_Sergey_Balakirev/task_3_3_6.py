e2ff33819416b0f004fe550fd41970b7ab73e6de
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-25 11:44:20 +0300

    task 3.3.6 Подвиг 6. Объявите класс с именем Complex для представления и работы с комплексными числами. Объекты этого класса должны создаваться командой:
    cm = Complex(real, img)
    где real - действительная часть комплексного числа (целое или вещественное значение); img - мнимая часть комплексного числа (целое или вещественное значение).
    
    Объявите в этом классе следующие объекты-свойства (property):
    
    real - для записи и считывания действительного значения;
    img - для записи и считывания мнимого значения.
    
    При записи новых значений необходимо проверять тип передаваемых данных. Если тип не соответствует целому или вещественному числу, то генерировать исключение командой:
    
    raise ValueError("Неверный тип данных.")
    Также с объектами класса Complex должна поддерживаться функция:
    
    res = abs(cm)
    возвращающая модуль комплексного числа (вычисляется по формуле: sqrt(real*real + img*img) - корень квадратный от суммы квадратов действительной и мнимой частей комплексного числа).
    
    Создайте объект cmp класса Complex для комплексного числа с real = 7 и img = 8. Затем, через объекты-свойства real и img измените эти значения на real = 3 и img = 4. Вычислите модуль полученного комплексного числа (сохраните результат в переменной c_abs).

---
+++ Python_OOP_training_course_Sergey_Balakirev/task_3_3_6.py
@@ -0,0 +1,36 @@
+class Complex:
+    def __init__(self, real: (int, float), img: (int, float)):
+        self.__real = real
+        self.__img = img
+
+    @property
+    def real(self):
+        return self.__real
+
+    @real.setter
+    def real(self, value):
+        self.__real = value
+
+    @property
+    def img(self):
+        return self.__img
+
+    @img.setter
+    def img(self, value):
+        self.__img = value
+
+    def __setattr__(self, key, value):
+        if key in ('real', 'img') and not isinstance(value, (int, float)):
+            raise ValueError("Неверный тип данных.")
+        super().__setattr__(key, value)
+
+    def __abs__(self):
+        return (self.real ** 2 + self.img ** 2) ** 0.5
+
+
+if __name__ == '__main__':
+    cmp = Complex(7, 8)
+    cmp.real = 3
+    cmp.img = 4
+    c_abs = abs(cmp)
+    print(c_abs)
