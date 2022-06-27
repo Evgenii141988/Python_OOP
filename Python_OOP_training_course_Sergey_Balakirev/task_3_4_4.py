4d3982cd139087f148e4ada47999d7372a97348c
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-27 16:30:53 +0300

    task 3.4.4 Подвиг 4. Известно, что в Python мы можем соединять два списка между собой с помощью оператора +:
    lst = [1, 2, 3] + [4.5, -3.6, 0.78]
    Но нет реализации оператора -, который бы убирал из списка все значения вычитаемого списка:
    
    lst = [1, 2, 3, 4, 5, 6] - [5, 6, 7, 8, 1] # [2, 3, 4] (порядок следования оставшихся элементов списка должен сохраняться)
    Давайте это поправим и создадим такой функционал. Для этого нужно объявить класс с именем NewList, объекты которого создаются командами:
    
    lst = NewList() # пустой список
    lst = NewList([-1, 0, 7.56, True]) # список с начальными значениями
    Реализуйте для этого класса работу с оператором вычитания, чтобы над объектами класса NewList можно было выполнять следующие действия:
    
    lst1 = NewList([1, 2, -4, 6, 10, 11, 15, False, True])
    lst2 = NewList([0, 1, 2, 3, True])
    res_1 = lst1 - lst2 # NewList: [-4, 6, 10, 11, 15, False]
    lst1 -= lst2 # NewList: [-4, 6, 10, 11, 15, False]
    res_2 = lst2 - [0, True] # NewList: [1, 2, 3]
    res_3 = [1, 2, 3, 4.5] - res_2 # NewList: [4.5]
    a = NewList([2, 3])
    res_4 = [1, 2, 2, 3] - a # NewList: [1, 2]
    Также в классе NewList необходимо объявить метод:
    
    get_list() - для возвращения результирующего списка объекта класса NewList
    
    Например:
    
    lst = res_2.get_list() # [1, 2, 3]

---
+++ Python_OOP_training_course_Sergey_Balakirev/task_3_4_4.py
@@ -0,0 +1,63 @@
+class NewList:
+    def __init__(self, lst: list = None):
+        self.lst = [] if lst is None else lst
+
+    def get_list(self):
+        return self.lst
+
+    @classmethod
+    def __get_new_list(cls, lst1, lst2):
+        d1 = [(i, type(i)) for i in lst1]
+        d2 = [(i, type(i)) for i in lst2]
+        for i in d2:
+            if i in d1:
+                d1.remove(i)
+        return [i[0] for i in d1]
+
+    def __sub__(self, other):
+        if not isinstance(other, (list, NewList)):
+            raise ArithmeticError("Операторы должны быть list или NewList")
+
+        second_lst = other
+        if isinstance(other, NewList):
+            second_lst = other.lst
+        return NewList(self.__get_new_list(self.lst, second_lst))
+
+    def __rsub__(self, other):
+        new_obj = NewList(other)
+        return new_obj - self
+
+    def __isub__(self, other):
+        if not isinstance(other, (list, NewList)):
+            raise ArithmeticError("Операторы должны быть list или NewList")
+
+        second_lst = other
+        if isinstance(other, NewList):
+            second_lst = other.lst
+
+        self.lst = self.__get_new_list(self.lst, second_lst)
+        return self
+
+
+if __name__ == '__main__':
+    lst1 = NewList([1, 2, -4, 6, 10, 11, 15, False, True])
+    lst2 = NewList([0, 1, 2, 3, True])
+    res_1 = lst1 - lst2  # NewList: [-4, 6, 10, 11, 15, False]
+    # print(res_1.get_list())
+    lst1 -= lst2  # NewList: [-4, 6, 10, 11, 15, False]
+    # print(lst1.get_list())
+    res_2 = lst2 - [0, True]  # NewList: [1, 2, 3]
+    print(res_2.get_list())
+    res_3 = [1, 2, 3, 4.5] - res_2  # NewList: [4.5]
+    print(f'res_2{res_2.get_list()}')
+    print(res_3.get_list())
+    # a = NewList([2, 3])
+    # res_4 = [1, 2, 2, 3] - a  # NewList: [1, 2]
+    # print(res_4.get_list())
+    # lst1 = NewList([0, 1, -3.4, "abc", True])
+    #
+    # lst2 = NewList()
+    # lst3 = lst1 - lst2
+    # lst4 = lst2 - []
+    # print(lst3.get_list())
+    # print(lst4.get_list())
