6c824c12313c6ca5422f4405cca1c09d9e6accea
Evgenii Zalesski <evgenii889111@gmail.com>
2022-06-27 16:31:26 +0300

    task 3.4.4 Подвиг 4. Известно, что в Python мы можем соединять два списка между собой с помощью оператора +:
    lst = [1, 2, 3] + [4.5, -3.6, 0.78]
    Но нет реализации оператора -, который бы убирал из списка все значения вычитаемого списка:
    
    lst = [1, 2, 3, 4, 5, 6] - [5, 6, 7, 8, 1] # [2, 3, 4] (порядок следования оставшихся элементов списка должен сохраняться)
    Давайте это поправим и создадим такой функционал. Для этого нужно объявить класс с именем NewList, объекты которого создаются командами:
    
    lst = NewList() # пустой список
    lst = NewList([-1, 0, 7.56, True]) # список с начальными значениями
    Реализуйте для этого класса работу с оператором вычитания, чтобы над объектами класса NewList можно было выполнять следующие действия:
    
    lst1 = NewList([1, 2, -4, 6, 10, 11, 15, False, True])
    lst2 = NewList([0, 1, 2, 3, True])
    res_1 = lst1 - lst2 # NewList: [-4, 6, 10, 11, 15, False]
    lst1 -= lst2 # NewList: [-4, 6, 10, 11, 15, False]
    res_2 = lst2 - [0, True] # NewList: [1, 2, 3]
    res_3 = [1, 2, 3, 4.5] - res_2 # NewList: [4.5]
    a = NewList([2, 3])
    res_4 = [1, 2, 2, 3] - a # NewList: [1, 2]
    Также в классе NewList необходимо объявить метод:
    
    get_list() - для возвращения результирующего списка объекта класса NewList
    
    Например:
    
    lst = res_2.get_list() # [1, 2, 3]

--- Python_OOP_training_course_Sergey_Balakirev/task_3_4_4.py
+++ Python_OOP_training_course_Sergey_Balakirev/task_3_4_4.py
@@ -43,21 +43,15 @@ if __name__ == '__main__':
     lst1 = NewList([1, 2, -4, 6, 10, 11, 15, False, True])
     lst2 = NewList([0, 1, 2, 3, True])
     res_1 = lst1 - lst2  # NewList: [-4, 6, 10, 11, 15, False]
-    # print(res_1.get_list())
+    print(res_1.get_list())
     lst1 -= lst2  # NewList: [-4, 6, 10, 11, 15, False]
-    # print(lst1.get_list())
+    print(lst1.get_list())
     res_2 = lst2 - [0, True]  # NewList: [1, 2, 3]
     print(res_2.get_list())
     res_3 = [1, 2, 3, 4.5] - res_2  # NewList: [4.5]
     print(f'res_2{res_2.get_list()}')
     print(res_3.get_list())
-    # a = NewList([2, 3])
-    # res_4 = [1, 2, 2, 3] - a  # NewList: [1, 2]
-    # print(res_4.get_list())
-    # lst1 = NewList([0, 1, -3.4, "abc", True])
-    #
-    # lst2 = NewList()
-    # lst3 = lst1 - lst2
-    # lst4 = lst2 - []
-    # print(lst3.get_list())
-    # print(lst4.get_list())
+    a = NewList([2, 3])
+    res_4 = [1, 2, 2, 3] - a  # NewList: [1, 2]
+    print(res_4.get_list())
+
